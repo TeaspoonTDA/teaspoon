02be1c5951d5616f446a248aaa2205b1479cf8bf
Liz Munch <muncheli@msu.edu>
2024-03-01 10:09:13 +0100

    Cleaned up documentation and added unit test

--- teaspoon/teaspoon/TDA/Magnitude.py
+++ teaspoon/teaspoon/TDA/Magnitude.py
@@ -1,6 +1,7 @@
 """
 This module provides algorithms related to computing the magnitude. See paper below for additional details 
-> Miguel O'Malley, Sara Kalisnik, Nina Otter. Alpha magnitude. Journal of Pure and Applied Algebra, Volume 227, Issue 11,2023, doi: [https://doi.org/10.1016/j.jpaa.2023.107396](10.1016/j.jpaa.2023.107396).
+
+- Miguel O'Malley, Sara Kalisnik, Nina Otter. Alpha magnitude. Journal of Pure and Applied Algebra, Volume 227, Issue 11,2023, doi: `10.1016/j.jpaa.2023.107396 <https://doi.org/10.1016/j.jpaa.2023.107396>`_.
 """
 
 import numpy as np
@@ -12,24 +13,22 @@ from numpy.linalg import inv
 
 
 def MagnitudeFunction(D,t_range = [.1,10], t_n = 100):
-    """This function calculates the magnitude function,  $t \\mapsto |tX|$,  of an input distance matrix assumed to be calculated from a finite metric space $X$, on the interval defined by `t_range` at `t_n` locations.
+    """
+    This function calculates the magnitude function,  ``t -> |tX|``,  of an input distance matrix assumed to be calculated from a finite metric space ``X``, on the interval defined by t_range at t_n locations.
 
-    - Given a finite metric space $(X,d)$ and for matrix purposes, fix an order on the points $x_1,\\cdots,x_n$.
-    - Denote the distance matrix by $D=[d(x_i,x_j)]_{ij}=[D_{ij}]_{ij}$
-    - Denote the similarity matrix $Z=Z_X$ to have entries $Z_{ij}=e^{-D_{ij}}$
-    - We'll also be interested in the scaled version for some $t \\in (0,\\infty)$, where $tZ$ is the matrix for metric space $tX$ and $tZ_{ij}=e^{-tD_{ij}}$
-    - The magnitude, in particular of $|tX|$ is 
-    $$ |tX| = \\sum_{i,j} ((tZ)^{-1})_{ij} $$
-    where $(tZ)^{-1}$ is the inverse of the matrix $tZ$, assuming it exists. 
-    - The magnitude function is 
-    $$M: t \\mapsto |tX|$$ 
+    - Given a finite metric space ``(X,d)`` and for matrix purposes, fix an order on the points ``x_1,\\cdots,x_n``.
+    - Denote the distance matrix by ``D=[d(x_i,x_j)]_{ij}=[D_{ij}]_{ij}``
+    - Denote the similarity matrix ``Z=Z_X`` to have entries ``Z_{ij}=e^{-D_{ij}}``
+    - We'll also be interested in the scaled version for some ``t \\in (0,\\infty)``, where ``tZ`` is the matrix for metric space ``tX`` and ``tZ_{ij}=e^{-tD_{ij}}``
+    - The magnitude, in particular of ``|tX|`` is ``|tX| = sum_{i,j} ((tZ)^{-1})_{ij}`` where ``(tZ)^{-1}`` is the inverse of the matrix ``tZ``, assuming it exists. 
+    - The magnitude function is ``M: t -> |tX|``
 
     Args:
         D (2-D array): 2-D square distance matrix
         t_range (length 2 list): 
 
     Returns:
-        T,M [1-D arrays]: T is the list of $t$ values, `M` is the list of associated values $|tX|$.
+        T,M [1-D arrays]: T is the list of ``t`` values, ``M`` is the list of associated values ``|tX|``.
     """
 
     # Get the list of locations for which we'll calculate the magintude. 
