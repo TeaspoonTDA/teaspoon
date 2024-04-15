c25356a70942c63c221466319ca34f1c5858eb08
danielle_barnes <barnesd8@msu.edu>
2024-04-15 11:10:21 -0400

    added feature functions module for all features

--- teaspoon/teaspoon/ML/feature_functions.py
+++ teaspoon/teaspoon/ML/feature_functions.py
@@ -20,6 +20,7 @@ from sympy import diff, integrate
 from itertools import combinations
 from numba import guvectorize
 from sklearn import mixture
+import copy
 
 # -------------------------------------------- #
 # -------------------------------------------- #
@@ -1299,79 +1300,6 @@ def kernel_distance(dgms0, dgms1, s, result):
         kSigma2 = kSigma2/(8 * np.pi * sigma)
         result[i] = math.sqrt(kSigma1 + kSigma0-2*kSigma2)
 
-@guvectorize(["void(int64[:,:,:], int64[:,:], float64, float64[:])",],"(p,m,n), (m,n), ()->(p)", target='cuda', nopython=True)
-def kernel_gpu(dgms0, dgms1, s, result):
-    """
-
-    This function computes (using gpu) the multiscale heat kernel distance for an array of persistence diagrams based on the formula provided in Ref. :cite:`5 <Reininghaus2015>`.
-    There are three inputs and these are two persistence diagram arrays and the kernel scale sigma.  This function should be used for large size datasets.  Please note this function will not work if you do not have a compatible GPU.
-    Details for compatibility can be found `here <https://numba.readthedocs.io/en/stable/cuda/index.html>`_.
-
-    Parameters
-    ----------
-    dgms0 : ndarray
-        Object array that includes first persistence diagram set.
-    dgms1 : ndarray
-        Object array that includes second persistence diagram set.
-    sigma : float
-        Kernel scale.
-
-    Returns
-    -------
-    result : np array
-        The kernel matrix
-
-    """
-    n_train = len(dgms0)
-    for i in range(n_train):
-        dgm0=dgms0[i]
-        dgm1 = dgms1
-        kSigma0 = 0
-        kSigma1 = 0
-        kSigma2 = 0
-        sigma = s
-        for k in range(dgm0.shape[0]):
-            p = dgm0[k,0:2]
-            if p[0]+p[1]==-2:
-                continue
-            for l in range(dgm0.shape[0]):
-                q = dgm0[l,0:2]
-                if q[0]+q[1]==-2:
-                    continue
-                qc = dgm0[l, 1::-1]
-                pq = (p[0] - q[0])**2 + (p[1] - q[1])**2
-                pqc = (p[0] - qc[0])**2 + (p[1] - qc[1])**2
-                kSigma0 += math.exp(-( pq) / (8 * sigma)) - math.exp(-(pqc) / (8 * sigma))
-        for k in range(dgm1.shape[0]):
-            p = dgm1[k,0:2]
-            if p[0]+p[1]==-2:
-                continue
-            for l in range(dgm1.shape[0]):
-                q = dgm1[l,0:2]
-                if q[0]+q[1]==-2:
-                    continue
-                qc = dgm1[l, 1::-1]
-                pq = (p[0] - q[0])**2 + (p[1] - q[1])**2
-                pqc = (p[0] - qc[0])**2 + (p[1] - qc[1])**2
-                kSigma1 += math.exp(-( pq) / (8 * sigma)) - math.exp(-(pqc) / (8 * sigma))
-        for k in range(dgm0.shape[0]):
-            p = dgm0[k,0:2]
-            if p[0]+p[1]==-2:
-                continue
-            for l in range(dgm1.shape[0]):
-                q = dgm1[l,0:2]
-                if q[0]+q[1]==-2:
-                    continue
-                qc = dgm1[l, 1::-1]
-                pq = (p[0] - q[0])**2 + (p[1] - q[1])**2
-                pqc = (p[0] - qc[0])**2 + (p[1] - qc[1])**2
-                kSigma2 += math.exp(-( pq) / (8 * sigma)) - math.exp(-(pqc) / (8 * sigma))
-
-        kSigma0 = kSigma0/(8 * np.pi * sigma)
-        kSigma1 = kSigma1/(8 * np.pi * sigma)
-        kSigma2 = kSigma2/(8 * np.pi * sigma)
-        result[i] = math.sqrt(kSigma1 + kSigma0-2*kSigma2)
-
 def f_dgm(dgm, function, **keyargs):
 	'''
 	Given a persistence diagram :math:`D = (S,\mu)` and a compactly supported function in :math:`\mathbb{R}^2`, this function computes
@@ -1454,15 +1382,14 @@ def adaptive_template_functions(train_dgms, d=25, threshold=.05):
     X_train_temp = np.vstack(train_dgms)
 	### return means for scoring
     gmm = mixture.BayesianGaussianMixture(n_components=d, covariance_type='full', max_iter=int(10e4)).fit(X_train_temp)
+    ellipses = []
+    for i in range(len(gmm.means_)):
+        L, v = np.linalg.eig(gmm.covariances_[i])
+        temp = {'mean':gmm.means_[i], 'std':np.sqrt(L), 'rotation':v.transpose(), 'radius':max(np.sqrt(L)), 'entropy':gmm.weights_[i]}
+        temp['std'] = 3*temp['std']
+        ellipses.append(temp)
 
-	ellipses = []
-	for i in range(len(gmm.means_)):
-		L, v = np.linalg.eig(gmm.covariances_[i])
-		temp = {'mean':gmm.means_[i], 'std':np.sqrt(L), 'rotation':v.transpose(), 'radius':max(np.sqrt(L)), 'entropy':gmm.weights_[i]}
-		temp['std'] = 3*temp['std']
-		ellipses.append(temp)
-
-	return ellipses
+    return ellipses
 
 def adaptive_template_features(list_dgms, list_ellipses, function=f_ellipse):
 	'''
@@ -1484,4 +1411,23 @@ def adaptive_template_features(list_dgms, list_ellipses, function=f_ellipse):
 		features[:,i] = adaptive_feature(list_dgms, function, **args)
 
 	return features
+
+def reshape_to_array(diagrams):
+    ndgms = copy.deepcopy(diagrams)
+    d0 = len(ndgms)
+    d1 = 0
+    d2 = 2
+    for i in range(0, d0):
+        d1 = max(d1, len(ndgms[i]))
+    for i in range(0,d0):
+        d = len(ndgms[i])
+        if d==d1:
+            continue
+        ndgms[i] = list(ndgms[i])
+        for k in range(0, d1-d):
+            ndgms[i].append([-1,-1])
+    dgms_array = []
+    for i in range(0, d0):
+        dgms_array.append(np.array(ndgms[i]))
+    return np.reshape(dgms_array, (d0, d1, d2))
 
--- tests/test_featureFunctions.py
+++ tests/test_featureFunctions.py
@@ -23,10 +23,19 @@ class TestFeatureFunctions(unittest.TestCase):
 
     def test_kernel_distance(self):
         mnist = load_datasets.mnist()
-        dgms = mnist['zero_dim_rtl'][0:3]
+        dgms = mnist['zero_dim_rtl'][0:2]
         sigma=.3
+        dgms = FF.reshape_to_array(dgms)
         k = FF.kernel_distance(dgms, dgms, sigma)
-        self.assertEqual(k.all(), np.array([4.83911125e+00, 1.12597993e-03, 2.64421959e-20, 2.15926035e+00,1.80429682e+00, 2.51962051e+01]).all())
+        self.assertEqual(k.all(), np.array([[0,.47224819],[.47224819,0]]).all())
+
+    def test_kernel_distance(self):
+        mnist = load_datasets.mnist()
+        dgms = mnist['zero_dim_rtl'][0:2]
+        sigma=.3
+        dgms = FF.reshape_to_array(dgms)
+        k = FF.kernel_distance_parallel(dgms, dgms, sigma)
+        self.assertEqual(k.all(), np.array([[0,.47224819],[.47224819,0]]).all())
 
 if __name__ == '__main__':
     unittest.main()
