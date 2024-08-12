d1fd4fc3e0f047d54d32114456944bbbf4b9f06f
maxchumley <chumleym@msu.edu>
2024-08-12 13:23:59 -0400

    Reorganize noise models and documentation

--- teaspoon/teaspoon/MakeData/DynSysLib/noise_models.py
+++ teaspoon/teaspoon/MakeData/DynSysLib/noise_models.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def noise_models(system, dynamic_state=None, L=None, fs=None,
                  SampleSize=None, parameters=None, InitialConditions=None):
     import numpy as np
@@ -27,7 +29,6 @@ def noise_models(system, dynamic_state=None, L=None, fs=None,
             ts = [(np.random.normal(mu, sigma, len(t)))[-SampleSize:]]
             t = t[-SampleSize:]
 
-    # In[ ]: Complete
 
         if system == 'uniform_noise':
             # setting simulation time series parameters
@@ -53,7 +54,6 @@ def noise_models(system, dynamic_state=None, L=None, fs=None,
             ts = [(np.random.uniform(a, b, size=len(t)))[-SampleSize:]]
             t = t[-SampleSize:]
 
-    # In[ ]: Complete
 
         if system == 'rayleigh_noise':
             # setting simulation time series parameters
@@ -79,7 +79,6 @@ def noise_models(system, dynamic_state=None, L=None, fs=None,
             ts = [(np.random.rayleigh(sigma, size=len(t)))[-SampleSize:]]
             t = t[-SampleSize:]
 
-    # In[ ]: Complete
 
         if system == 'exponential_noise':
             # setting simulation time series parameters
@@ -104,4 +103,92 @@ def noise_models(system, dynamic_state=None, L=None, fs=None,
 
             ts = [(np.random.exponential(sigma, len(t)))[-SampleSize:]]
             t = t[-SampleSize:]
+    return t, ts
+
+
+
+def gaussian_noise(sigma=1.0, mu=0.0, 
+         L=1000, fs=1, SampleSize=1000):
+    """
+    Add description from `Audun's pdf <https://teaspoontda.github.io/teaspoon/_downloads/8d622bebe5abdc608bbc9616ffa444d9/dynamic_systems_library.pdf>`_
+
+    Parameters:
+        sigma (Optional[float]): Standard deviation of the normal distribution.
+        mu (Optional[float]): Mean of the normal distribution. 
+        L (Optional[int]): amount of time to solve simulation for.
+        fs (Optional[int]): sampling rate for simulation.
+        SampleSize (Optional[int]): length of sample at end of entire time series
+
+    Returns:
+        array: Array of the time indices as `t` and the simulation time series `ts`
+    """
+
+    t = np.linspace(0, L, int(L*fs))
+    ts = [(np.random.normal(mu, sigma, len(t)))[-SampleSize:]]
+    t = t[-SampleSize:]
+
+    return t, ts
+
+def uniform_noise(a=-1.0, b=1.0, 
+         L=1000, fs=1, SampleSize=1000):
+    """
+    Add description from `Audun's pdf <https://teaspoontda.github.io/teaspoon/_downloads/8d622bebe5abdc608bbc9616ffa444d9/dynamic_systems_library.pdf>`_
+
+    Parameters:
+        a (Optional[float]): Uniform distribution lower bound.
+        b (Optional[float]): Uniform distribution upper bound. 
+        L (Optional[int]): amount of time to solve simulation for.
+        fs (Optional[int]): sampling rate for simulation.
+        SampleSize (Optional[int]): length of sample at end of entire time series
+
+    Returns:
+        array: Array of the time indices as `t` and the simulation time series `ts`
+    """
+
+    t = np.linspace(0, L, int(L*fs))
+    ts = [(np.random.uniform(a, b, size=len(t)))[-SampleSize:]]
+    t = t[-SampleSize:]
+
+    return t, ts
+
+def rayleigh_noise(sigma=1.0, 
+         L=1000, fs=1, SampleSize=1000):
+    """
+    Add description from `Audun's pdf <https://teaspoontda.github.io/teaspoon/_downloads/8d622bebe5abdc608bbc9616ffa444d9/dynamic_systems_library.pdf>`_
+
+    Parameters:
+        sigma (Optional[float]): Rayleigh distribution mode.
+        L (Optional[int]): amount of time to solve simulation for.
+        fs (Optional[int]): sampling rate for simulation.
+        SampleSize (Optional[int]): length of sample at end of entire time series
+
+    Returns:
+        array: Array of the time indices as `t` and the simulation time series `ts`
+    """
+
+    t = np.linspace(0, L, int(L*fs))
+    ts = [(np.random.rayleigh(sigma, size=len(t)))[-SampleSize:]]
+    t = t[-SampleSize:]
+
+    return t, ts
+
+def exponential_noise(sigma=1.0, 
+         L=1000, fs=1, SampleSize=1000):
+    """
+    Add description from `Audun's pdf <https://teaspoontda.github.io/teaspoon/_downloads/8d622bebe5abdc608bbc9616ffa444d9/dynamic_systems_library.pdf>`_
+
+    Parameters:
+        sigma (Optional[float]): Exponential distribution scale.
+        L (Optional[int]): amount of time to solve simulation for.
+        fs (Optional[int]): sampling rate for simulation.
+        SampleSize (Optional[int]): length of sample at end of entire time series
+
+    Returns:
+        array: Array of the time indices as `t` and the simulation time series `ts`
+    """
+
+    t = np.linspace(0, L, int(L*fs))
+    ts = [(np.random.exponential(sigma, len(t)))[-SampleSize:]]
+    t = t[-SampleSize:]
+
     return t, ts
