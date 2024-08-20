645e1909a60e1c2e7f729b26f3bca8c1cc1ee9ce
stanweer1 <114263384+stanweer1@users.noreply.github.com>
2024-08-20 14:04:30 -0400

    Delayed flows and medical data

--- teaspoon/teaspoon/MakeData/DynSysLib/delayed_flows.py
+++ teaspoon/teaspoon/MakeData/DynSysLib/delayed_flows.py
@@ -1,49 +1,70 @@
+import numpy as np
+
 def delayed_flows(system, dynamic_state=None, L=None, fs=None,
                   SampleSize=None, parameters=None, InitialConditions=None):
-    import numpy as np
-    run = True
-    if run == True:
-        if system == 'mackey_glass':
-            # This requires installation of ddeint (pip install ddeint)
-            from ddeint import ddeint
-            # setting simulation time series parameters
-            if fs == None:
-                fs = 5
-            if SampleSize == None:
-                SampleSize = 1000
-            if L == None:
-                L = 400
-            t = np.linspace(0, L, int(L*fs))
 
-            # setting system parameters
-            if parameters != None:
-                if len(parameters) != 4:
-                    print(
-                        'Warning: needed 4 parameters. Defaulting to periodic solution parameters.')
-                    parameters = None
-                else:
-                    gamma, τ, B, n = parameters[0], parameters[1], parameters[2], parameters[3]
-            if parameters == None:
-                if dynamic_state == 'periodic':
-                    n = 7.75
-                if dynamic_state == 'chaotic':
-                    n = 9.65
-                τ = 2.0
-                B = 2.0
-                gamma = 1.0001
+    if system == 'mackey_glass':
+        t, ts = mackey_glass()
 
-            def mackey_glass(X, t, d):
-                x = X(t)
-                xd = X(t-d)
-                return B*(xd/(1+xd**n)) - gamma*x
+    return t, ts
 
-            fsolve = 50
-            tt = np.linspace(0, L, int(L*fsolve))
-            def g(t): return np.array([1, 1])
-            d = τ
-            states = ddeint(mackey_glass, g, tt, fargs=(d,)).T
+def mackey_glass(parameters=None, fs=5, SampleSize=1000, L=400, dynamic_state='periodic'):
+    """
+    The Mackey-Glass Delayed Differential Equation is
 
-            ts = [((states[0])[::int(fsolve/fs)])[-SampleSize:]]
-            t = tt[::int(fsolve/fs)][-SampleSize:]
+    .. math::
+        \dot{x}(t) = -\\gamma*x(t) + \\beta*\\frac{x(t - \\tau)}{1 + x(t - \\tau)^n}
+    
+    where we chose the parameters :math:`\\tau = 2, \\beta = 2, \\gamma = 1,` and :math:`n = 9.65`. We solve this system for 400 seconds with a sampling rate of 50 Hz. The solution was then downsampled to 5 Hz and the last 200 seconds were used for the figure.
+
+    .. figure:: ../../../figures/Delayed_Flows/Mackey_Glass_Delayed_Differential_Equation.png
+    
+    Parameters:
+        parameters (Optional[floats]): Array of four floats [gamma, tau, beta, n]
+        fs (Optional[float]): Sampling rate for simulation
+        SampleSize (Optional[int]): length of sample at end of entire time series
+        L (Optional[int]): Number of iterations
+        dynamic_state (Optional[str]): Set dynamic state as either 'periodic' or 'chaotic' is not supplying parameters.
+        
+    Returns:
+        array: Array of the time indices as `t` and the simulation time series `ts`
+    
+    """
+
+    # This requires installation of ddeint (pip install ddeint)
+    from ddeint import ddeint
+
+    t = np.linspace(0, L, int(L*fs))
+
+    # setting system parameters
+    if len(parameters) != 4:
+        print(
+            'Warning: needed 4 parameters. Defaulting to periodic solution parameters.')
+        parameters = None
+    else:
+        gamma, τ, B, n = parameters[0], parameters[1], parameters[2], parameters[3]
+
+    if parameters == None:
+        if dynamic_state == 'periodic':
+            n = 7.75
+        if dynamic_state == 'chaotic':
+            n = 9.65
+        τ = 2.0
+        B = 2.0
+        gamma = 1.0001
+
+    def mackey_glass(X, t, d):
+        x = X(t)
+        xd = X(t-d)
+        return B*(xd/(1+xd**n)) - gamma*x
+
+    fsolve = 50
+    tt = np.linspace(0, L, int(L*fsolve))
+    def g(t): return np.array([1, 1])
+    d = τ
+    states = ddeint(mackey_glass, g, tt, fargs=(d,)).T
+
+    ts = [((states[0])[::int(fsolve/fs)])[-SampleSize:]]
+    t = tt[::int(fsolve/fs)][-SampleSize:]
 
     return t, ts
--- teaspoon/teaspoon/MakeData/DynSysLib/medical_data.py
+++ teaspoon/teaspoon/MakeData/DynSysLib/medical_data.py
@@ -1,39 +1,80 @@
+import numpy as np
+
 def medical_data(system, dynamic_state=None, L=None, fs=None,
                  SampleSize=None, parameters=None, InitialConditions=None):
-    import numpy as np
-    run = True
-    if run == True:
-        if system == 'ECG':
-            from scipy.misc import electrocardiogram
 
-            if dynamic_state == 'periodic':  # healthy
-                ts = [electrocardiogram()[3000:5500]]
+    if system == 'ECG':
+        t, ts = ECG()
 
-            if dynamic_state == 'chaotic':  # heart arrythmia
-                ts = [electrocardiogram()[8500:11000]]
+    if system == 'EEG':
+        t, ts = EEG()
 
-            fs = 360
-            ts = [(ts[0])]
-            t = np.arange(len(ts[0]))/fs
+    return t, ts
 
-    # In[ ]: Complete
+def EEG(SampleSize=5000, dynamic_state='normal'):
 
-        if system == 'EEG':
+    """
+    The EEG signal was taken from andrzejak et al. [1]. Speci cally, the first 5000 data points from the EEG data of a healthy patient from set A (file Z-093) was used and the first 5000 data points of a patient experiencing a seizure from set E (file S-056) was used (see figure below for case during seizure).
 
-            if SampleSize == None:
-                SampleSize = 5000
+    .. figure:: ../../../figures/Human_Medical_Data/EEG_Data.png
+    
+    Parameters:
+        SampleSize (Optional[int]): length of sample at end of entire time series
+        dynamic_state (Optional[str]): Set dynamic state as either 'normal' or 'seizure'.
+        
+    Returns:
+        array: Array of the time indices as `t` and the simulation time series `ts`
 
-            if dynamic_state == 'periodic':  # healthy
-                ts = [np.loadtxt('Data\\EEG\\Z093.txt', skiprows=1)[
-                    0:SampleSize]]
+    References
+    ----------
+    .. [1] Ralph G Andrzejak, Klaus Lehnertz, Florian Mormann, Christoph Rieke, Peter David, and Christian E Elger. Indications of nonlinear deterministic and nite-dimensional structures in time series of brain electrical activity: Dependence on recording region and brain state. Physical Review E, 64(6):061907, 2001.
+    
+    """
 
-            if dynamic_state == 'chaotic':  # seizure
-                ts = [np.loadtxt('Data\\EEG\\S056.txt', skiprows=1)[
-                    0:SampleSize]]
+    if dynamic_state == 'normal':  # healthy
+        ts = [np.loadtxt('Data\\EEG\\Z093.txt', skiprows=1)[
+            0:SampleSize]]
 
-            fs = 173.61
-            t = np.arange(len(ts[0]))/fs
-            t = t[-SampleSize:]
-            ts = [(ts[0])[-SampleSize:]]
+    if dynamic_state == 'seizure':  # seizure
+        ts = [np.loadtxt('Data\\EEG\\S056.txt', skiprows=1)[
+            0:SampleSize]]
+
+    fs = 173.61
+    t = np.arange(len(ts[0]))/fs
+    t = t[-SampleSize:]
+    ts = [(ts[0])[-SampleSize:]]
+
+    return t, ts
+
+def ECG(dynamic_state='normal'):
+
+    """
+    The Electrocardoagram (ECG) data was taken from SciPys misc.electrocardiogram data set. This ECG data was originally provided by the MIT-BIH Arrhythmia Database [1]. We used data points 3000 to 5500  during normal sinus rhythm and 8500 to 11000 during arrhythmia (arrhythmia case shown below in figure).
+
+    .. figure:: ../../../figures/Human_Medical_Data/ECG_Data.png
+    
+    Parameters:
+        dynamic_state (Optional[str]): Set dynamic state as either 'normal' or 'seizure'.
+        
+    Returns:
+        array: Array of the time indices as `t` and the simulation time series `ts`
+
+    References
+    ----------
+    .. [1] George B Moody and Roger G Mark. Theimpact of the mit-bih arrhythmia database. IEEE Engineering in Medicine and Biology Magazine, 20(3):4550, 2001.
+    
+    """
+
+    from scipy.misc import electrocardiogram
+
+    if dynamic_state == 'normal':  # healthy
+        ts = [electrocardiogram()[3000:5500]]
+
+    if dynamic_state == 'seizure':  # heart arrythmia
+        ts = [electrocardiogram()[8500:11000]]
+
+    fs = 360
+    ts = [(ts[0])]
+    t = np.arange(len(ts[0]))/fs
 
     return t, ts
