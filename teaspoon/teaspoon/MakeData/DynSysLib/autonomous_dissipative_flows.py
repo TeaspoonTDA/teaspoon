b1a6ea2df76b040c1245fd6ab4e9b6c86eaeafd9
maxchumley <chumleym@msu.edu>
2024-08-23 14:03:44 -0400

    Revert "Update autonomous_dissipative_flows.py"
    This reverts commit ea14890fa6bc6a06731c360d0175d0065ccd84f3.

--- teaspoon/teaspoon/MakeData/DynSysLib/autonomous_dissipative_flows.py
+++ teaspoon/teaspoon/MakeData/DynSysLib/autonomous_dissipative_flows.py
@@ -323,7 +323,43 @@ def autonomous_dissipative_flows(system, dynamic_state=None, L=None, fs=None,
 # Max
 
     if system == 'burke_shaw_attractor':
-        t, ts = burke_shaw_attractor()
+        # system from http://www.atomosyd.net/spip.php?article33
+        # setting simulation time series parameters
+        if fs == None:
+            fs = 200
+        if SampleSize == None:
+            SampleSize = 5000
+        if L == None:
+            L = 500.0
+        t = np.linspace(0, L, int(L*fs))
+
+        # setting system parameters
+        if parameters != None:
+            if len(parameters) != 2:
+                print(
+                    'Warning: needed 2 parameters. Defaulting to periodic solution parameters.')
+                parameters = None
+            else:
+                s, V = parameters[0], parameters[1]
+        if parameters == None:
+            if dynamic_state == 'periodic':
+                s = 12
+            if dynamic_state == 'chaotic':
+                s = 10
+            V = 4
+
+        # defining simulation functions
+        def burke_shaw_attractor(state, t):
+            x, y, z = state  # unpack the state vector
+            return -s*(x+y), -y - s*x*z, s*x*y + V
+
+        if InitialConditions == None:
+            InitialConditions = [0.6, 0, 0]
+
+        states = odeint(burke_shaw_attractor, InitialConditions, t)
+        ts = [(states[:, 0])[-SampleSize:], (states[:, 1])
+                [-SampleSize:], (states[:, 2])[-SampleSize:]]
+        t = t[-SampleSize:]
 
 
     if system == 'rucklidge_attractor':
@@ -563,8 +599,6 @@ def autonomous_dissipative_flows(system, dynamic_state=None, L=None, fs=None,
         t = t[-SampleSize:]
 
     return t, ts
-
-
 
 
 
@@ -1186,73 +1220,6 @@ def chens_system(parameters=[30.0,3.0,28.0], dynamic_state=None, InitialConditio
         InitialConditions = [-10, 0, 37]
 
     states = odeint(chens_system, InitialConditions, t)
-    ts = [(states[:, 0])[-SampleSize:], (states[:, 1])
-            [-SampleSize:], (states[:, 2])[-SampleSize:]]
-    t = t[-SampleSize:]
-
-    return t, ts
-
-
-def burke_shaw_attractor(parameters=[12.0,4.0], dynamic_state=None, InitialConditions=[0.6,0.0,0.0], L=500.0, fs=200, SampleSize=5000):
-    """
-    The Burke-Shaw Attractor is defined [8]_ as 
-
-    .. math::
-        \dot{x} &= s(x+y),
-
-        \dot{y} &= -y - sxz,
-
-        \dot{z} &= sxy + V
-
-    The system parameters are set to :math:`s = 12`, :math:`V = 4`, and :math:`c = 28` for a periodic response and :math:`s = 10` for a chaotic response. The initial conditions were set to :math:`[x, y, z] = [0.6,0.0,0.0]`. The system was simulated for 500 seconds at a rate of 200 Hz and the last 25 seconds were used for the chaotic response.
-
-    .. figure:: ../../../figures/Autonomous_Dissipative_Flows/Burke_Shaw_Attractor.png
-
-    Parameters:
-        parameters (Optional[floats]): Array of one float [:math:`s`, :math:`V`] or None if using the dynamic_state variable
-        fs (Optional[float]): Sampling rate for simulation
-        SampleSize (Optional[int]): length of sample at end of entire time series
-        L (Optional[int]): Number of iterations
-        InitialConditions (Optional[floats]): list of values for [:math:`x_0`, :math:`y_0`, :math:`z_0`]
-        dynamic_state (Optional[str]): Set dynamic state as either 'periodic' or 'chaotic' if not supplying parameters.
-
-    Returns:
-        array: Array of the time indices as `t` and the simulation time series `ts`
-
-    References
-    ----------
-    .. [8] Shaw, Robert. "Strange attractors, chaotic behavior, and information flow". Zeitschrift für Naturforschung, 1981.
-
-    """
-    # system from http://www.atomosyd.net/spip.php?article33
-
-    t = np.linspace(0, L, int(L*fs))
-
-    # setting system parameters
-    num_param = 2
-
-    if len(parameters) != num_param:
-        raise ValueError(f'Need {num_param} parameters as specified in documentation.')
-    elif dynamic_state != None:
-        if dynamic_state == 'periodic':
-            s = 12.0
-        elif dynamic_state == 'chaotic':
-            s = 10.0
-        else:
-            raise ValueError(f'dynamic_state needs to be either "periodic" or "chaotic" or provide an array of length {num_param} in parameters.')
-        V = 4.0
-    else:
-        s, V = parameters[0], parameters[1]
-
-    # defining simulation functions
-    def burke_shaw_attractor(state, t):
-        x, y, z = state  # unpack the state vector
-        return -s*(x+y), -y - s*x*z, s*x*y + V
-
-    if InitialConditions == None:
-        InitialConditions = [0.6, 0, 0]
-
-    states = odeint(burke_shaw_attractor, InitialConditions, t)
     ts = [(states[:, 0])[-SampleSize:], (states[:, 1])
             [-SampleSize:], (states[:, 2])[-SampleSize:]]
     t = t[-SampleSize:]
