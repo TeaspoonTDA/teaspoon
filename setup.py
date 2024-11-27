eca393c7289878c4a0b97ec5ba390358ffce840a
maxchumley <chumleym@msu.edu>
2024-11-27 09:16:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,4 +9,7 @@ if __name__ == "__main__":
     setup(packages = find_packages(),
     long_description = ld,
     dependency_links = ["git+https://github.com/shizuo-kaji/CubicalRipser_3dim.git"],
+    package_data={
+        'teaspoon.MakeData.DynSysLib.Data.EEG': ['Z093.txt'],
+    },
     include_package_data=True)
