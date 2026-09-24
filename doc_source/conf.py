454be801d151e3e1fee45cf7cb073ac71abbeca0
ismailguzel <ismailgzel@gmail.com>
2026-09-24 11:47:30 -0400

    Bump package version to 1.6.1

--- doc_source/conf.py
+++ doc_source/conf.py
@@ -19,7 +19,7 @@ sys.modules['ripser'] = mock.Mock()
 project = 'teaspoon'
 copyright = '2024, Munch and Khasawneh'
 author = 'Elizabeth Munch and Firas Khasawneh'
-release = '1.6.0'
+release = '1.6.1'
 
 source_suffix = {
     '.rst': 'restructuredtext',
